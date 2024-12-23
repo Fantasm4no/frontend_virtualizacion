3547cd00d59204d2d897b1906c1ca5b4d6408352
Fantasm4no <erickfernando_11@hotmail.com>
2024-12-22 23:54:20 -0500

    Ajustes modelo Book

--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -1,12 +1,13 @@
 export class Book {
-    id!: number; // El operador `!` indica que esta propiedad será inicializada posteriormente
-    title!: string;
-    author!: string;
-  
-    constructor(id: number, title: string, author: string) {
-      this.id = id;
-      this.title = title;
-      this.author = author;
-    }
+  id!: number; // El operador `!` indica que esta propiedad será inicializada posteriormente
+  title!: string;
+  author!: string;
+  image_url?: string; // Propiedad opcional para la URL de la imagen
+
+  constructor(id: number, title: string, author: string, image_url?: string) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.image_url = image_url; // Inicializamos la URL de la imagen si está disponible
   }
-  
+}
